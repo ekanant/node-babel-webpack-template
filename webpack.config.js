a458a0abb8011111f2c9f5af492f1802802738ea
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2018-11-10 11:14:59 +0700

    Finish with webpack template

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+    mode: 'production',
+    target: 'node',
+    entry: {
+        index: __dirname + '/src/index.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [new CleanWebpackPlugin(['dist'])],
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+}
